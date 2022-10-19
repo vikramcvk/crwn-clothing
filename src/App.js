915b19c8a0bee1bbafd19e77c02d04795a1a0b92
Vikram Kumar Chindam <vikramkumarchindam@Vikrams-MacBook-Pro.local>
2022-10-20 05:26:36 +0530

    5th step added directory component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
 import './categories.styles.scss';
 
 const App = () => 
@@ -36,20 +36,9 @@ const App = () =>
   
 
 
-return  (
-
-<div className = "categories-container">
-
-  {categories.map((category) => ( 
-    <CategoryItem key={category.id} category={category} />
-
-        
-  ))}
-     
-  </div>
-
-  );
-
+return   <Directory categories={categories} /> ;
+  
 }
+  
 
 export default App;
