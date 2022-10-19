7ad0f1b3b8dff15145aa6ca053c998c0b5a24e10
Vikram Kumar Chindam <vikramkumarchindam@Vikrams-MacBook-Pro.local>
2022-10-19 12:40:54 +0530

    3rd step: Added image urls and complete html and scss code as monolithic code

--- src/App.js
+++ src/App.js
@@ -5,42 +5,45 @@ const App = () =>
 {
 
   const categories = 
+
   [
-
-          {
-            id: 1,
-            title: 'Hats',
-          },
-
-          {
-            id: 2,
-            title: 'Jackets',
-          },
-          {
-            id: 3,
-            title: 'Sweaters',
-          },
-          {
-            id: 4,
-            title: 'Mens',
-          },
-          {
-            id: 5,
-            title: 'Womans',
-          },
-
-
-
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ];
+  
+
 
 return  (
 
 <div className = "categories-container">
 
-  {categories.map(({title}) => ( 
+  {categories.map(({title,id,imageUrl}) => ( 
 
           <div className = "category-container">
-                <div className = "background-image" />
+                <div className = "background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
                   <div className= "category-body-container">
 
                       <h2>{title}</h2>
