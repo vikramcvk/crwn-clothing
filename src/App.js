ccd9c1a637672aab05cf41228e314fbe5c604d5c
Vikram Kumar Chindam <vikramkumarchindam@Vikrams-MacBook-Pro.local>
2022-10-19 10:54:51 +0530

    sass file created and output as expected

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
-//import './categories.styles.css';
-import './categorystyles.scss';
+import './categories.styles.scss';
 
 const App = () => 
 {
@@ -36,13 +35,13 @@ const App = () =>
 
 return  (
 
-<div className = "categories.container">
+<div className = "categories-container">
 
   {categories.map(({title}) => ( 
 
           <div className = "category-container">
                 <div className = "background-image" />
-                  <div className = "category-body-container">
+                  <div className= "category-body-container">
 
                       <h2>{title}</h2>
                       <p>Shop Now </p>
